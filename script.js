12fdce214f88ffb22a6bae1bff79394fe7aaa5f2
Sandra Felicia <mjpfelicia@gmail.com>
2025-01-23 15:39:41 -0300

    criando projeto

---
+++ script.js
@@ -0,0 +1,9 @@
+document.addEventListener('DOMContentLoaded', function () {
+    const numberContainer = document.getElementById('numberContainer');
+    for (let i = 1; i <= 50; i++) {
+        const numberDiv = document.createElement('div');
+        numberDiv.className = 'number';
+        numberDiv.textContent = i;
+        numberContainer.appendChild(numberDiv);
+    }
+});
